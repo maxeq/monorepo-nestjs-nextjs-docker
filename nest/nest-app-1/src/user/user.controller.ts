8a5b31c0a7d1522496ef128db071a623c557e875
maxeq <maxeq1@gmail.com>
2024-09-04 18:41:54 +0200

    Update user.controller.ts

--- nest/nest-app-1/src/user/user.controller.ts
+++ nest/nest-app-1/src/user/user.controller.ts
@@ -9,11 +9,13 @@ import {
   Param,
   ParseUUIDPipe,
   Put,
+  UnauthorizedException,
   UseInterceptors,
 } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { UserResponse } from "./responses";
 import { UserService } from "./user.service";
+import { compareSync } from "bcryptjs";
 
 @Controller("user")
 export class UserController {
@@ -43,5 +45,29 @@ export class UserController {
   async updateUser(@Body() body: Partial<User>) {
     const user = await this.userService.save(body);
     return new UserResponse(user);
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Put("change-password")
+  async changePassword(
+    @Body() body: { currentPassword: string; newPassword: string },
+    @CurrentUser() currentUser: JwtPayload, // Извлечение текущего пользователя из JWT
+  ) {
+    // Извлекаем пользователя из базы данных по его ID из JWT
+    const user = await this.userService.findOne(currentUser.id);
+
+    // Проверяем, что текущий пароль совпадает с тем, что хранится в базе
+    if (!compareSync(body.currentPassword, user.password)) {
+      throw new UnauthorizedException("Current password is incorrect");
+    }
+
+    // Обновляем пароль пользователя в базе данных
+    user.password = body.newPassword;
+
+    // Сохраняем изменения в базе данных
+    const updatedUser = await this.userService.save(user);
+
+    // Возвращаем обновлённого пользователя
+    return new UserResponse(updatedUser);
   }
 }
